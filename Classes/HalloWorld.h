4b733619570e1939e522421bda27a0e779b3939a
Igor Gun <igor.gun@idealo.de>
2016-04-27 11:11:19 +0200

    initial commit

---
+++ Classes/HalloWorld.h
@@ -0,0 +1,18 @@
+//
+//  HalloWorld.h
+//  TestFramework
+//
+//  Created by Igor Gun on 22.04.16.
+//  Copyright © 2016 Igor Gun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//test
+@interface HalloWorld : NSObject
+
+- (void) foo;
+- (void) foo2;
+- (void) foo3;
+
+@end
---
+++ Classes/HalloWorld.m
@@ -0,0 +1,28 @@
+//
+//  HalloWorld.m
+//  TestFramework
+//
+//  Created by Igor Gun on 22.04.16.
+//  Copyright © 2016 Igor Gun. All rights reserved.
+//
+
+#import "HalloWorld.h"
+
+@implementation HalloWorld
+
+- (void) foo
+{
+    NSLog(@"hallo world");
+}
+
+- (void) foo2
+{
+    NSLog(@"hallo world - foo2");
+}
+
+- (void) foo3
+{
+    NSLog(@"hallo world - foo3");
+}
+
+@end
