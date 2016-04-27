16cc844441556587010c5838f38a0f158d3f34a8
Igor Gun <igor.gun@idealo.de>
2016-04-27 10:55:33 +0200

    Initial Commit

---
+++ Module1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Module1
+//
+//  Created by Igor Gun on 27.04.16.
+//  Copyright © 2016 Igor Gun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Module1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Module1
+//
+//  Created by Igor Gun on 27.04.16.
+//  Copyright © 2016 Igor Gun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
